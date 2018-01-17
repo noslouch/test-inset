6e767bde08f2ecb5f9c83a41e248d2ef7fd3f48c
Dov Friedman <dov.friedman@wsj.com>
2018-01-17 10:37:56 -0500

    webpacking

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const webpack = require('webpack');
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+const cssName = isProduction ? '[name].min.css' : '[name].css';
+const extractCSS = new ExtractTextPlugin(cssName);
+
+const options = {
+  devtool: 'source-map',
+  entry: {
+    app: './src/index.js'
+  },
+  output: {
+    filename: isProduction ? '[name].min.js' : '[name].js',
+    path: path.resolve(__dirname, 'build')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader','postcss-loader','sass-loader']
+        })
+      },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['babel-preset-env']
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    extractCSS
+  ]
+};
+
+if(isProduction){
+  options.plugins.push(new webpack.DefinePlugin({
+    'process.env': {
+      'NODE_ENV': JSON.stringify('production')
+    }
+  }))
+}
+
+module.exports = options;
