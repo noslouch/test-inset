e9e5da4490f418a8491d59cfeeff10bda6761acb
Tynan DeBold <tynan.debold@wsj.com>
2018-02-05 10:43:48 -0500

    persistent layout

--- libs/js/index.js
+++ libs/js/index.js
@@ -8,5 +8,18 @@
 
     $inset.classList.remove('inline', 'wrap', 'margin', 'offset', 'bleed');
     $inset.classList.add(this.value);
+
+    modifyUrl();
   }
+
+  function modifyUrl(){
+    history.pushState('', '', window.location.pathname);
+    window.location.href = window.location.href + '#' + $select.value;
+  }
+
+  $select.value = window.location.hash.substr(1);
+  var event = new Event('change');
+  $select.dispatchEvent(event);
+
+  modifyUrl();
 }());
