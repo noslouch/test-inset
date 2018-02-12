edda1ad392992c8ffbad391ab2019a7af8358b1b
Tynan DeBold <tynan.debold@wsj.com>
2018-02-12 09:24:15 -0500

    better logic for persistent layout

--- libs/js/index.js
+++ libs/js/index.js
@@ -3,6 +3,17 @@
   var $inset  = document.querySelector('.media-object');
   $select.addEventListener('change', changeLayout);
 
+  if (!window.location.hash) {
+    history.pushState('', '', window.location.pathname);
+    window.location.href = window.location.href + '#inline';
+  } else {
+    $select.value = window.location.hash.substr(1);
+
+    // run changeLayout()
+    var event = new Event('change');
+    $select.dispatchEvent(event);
+  }
+
   function changeLayout(e) {
     e.preventDefault();
 
@@ -16,10 +27,4 @@
     history.pushState('', '', window.location.pathname);
     window.location.href = window.location.href + '#' + $select.value;
   }
-
-  $select.value = window.location.hash.substr(1);
-  var event = new Event('change');
-  $select.dispatchEvent(event);
-
-  modifyUrl();
 }());
